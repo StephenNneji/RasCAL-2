0e3c36b5bccb809c9ccdead32a635f3bd40f7d05
Stephen Nneji <steve.nneji@gmail.com>
2026-03-13 12:52:47 +0000

    Linux

--- rascal2/main.py
+++ rascal2/main.py
@@ -2,11 +2,23 @@ import multiprocessing
 import os
 import sys
 
-from PyQt6.QtCore import Qt
+from PyQt6.QtCore import Qt, QThread
 from PyQt6.QtGui import QIcon, QPixmap
 from PyQt6.QtWidgets import QApplication, QSplashScreen
 
 from rascal2.paths import path_for
+
+
+class SplashScreen(QSplashScreen):
+    """Create splash screen widget."""
+
+    def __init__(self, *args):
+        super().__init__(*args)
+        self.painted = False
+
+    def paintEvent(self, event):
+        super().paintEvent(event)
+        self.painted = True
 
 
 def main():
@@ -16,11 +28,18 @@ def main():
     app = QApplication([])
     app.setWindowIcon(QIcon(path_for("logo.png")))
 
-    splash = QSplashScreen(QPixmap(path_for("splash.png")), Qt.WindowType.WindowStaysOnTopHint)
+    splash = SplashScreen(QPixmap(path_for("splash.png")), Qt.WindowType.WindowStaysOnTopHint)
     splash.setAttribute(Qt.WidgetAttribute.WA_DeleteOnClose)
     splash.show()
     splash.raise_()
     splash.activateWindow()
+    app.processEvents()
+    for _ in range(100):
+        if splash.painted:
+            break
+        # wait for splash to paint on linux
+        QThread.usleep(100)
+        app.processEvents()
     app.processEvents()
 
     os.environ["DELAY_MATLAB_START"] = "1"
