0ccd5ebc35d3fa2933c80539de154d3dc11aa605
Stephen Nneji <steve.nneji@gmail.com>
2026-06-18 14:27:06 +0100

    Add macOS signing code

--- packaging/macos/make.sh
+++ packaging/macos/make.sh
@@ -3,6 +3,7 @@
 RASCAL_PATH="../bundle/rascal.app"
 VER_NAME=$1
 ARCH_NAME=$2
+DEV_TEAM_ID=$3
 VER="main"
 
 if [[ ${VER_NAME:0:1} == 'v' ]]; then
@@ -15,7 +16,11 @@ else
     NAME="RasCAL-2-${VER}-macos-${ARCH_NAME}.pkg"
 fi
 
+# Sign code
+codesign -v --deep --force --options=runtime --entitlements ./entitlements.plist --sign ${DEV_TEAM_ID} --timestamp ${RASCAL_PATH}/Contents/Resources/*.dylib
+codesign -v --deep --force --options=runtime --entitlements ./entitlements.plist --sign ${DEV_TEAM_ID} --timestamp ${RASCAL_PATH}
+
 # Build Pkg
 sed -e "s/@VERSION_NAME@/${VER_NAME}/g" -e "s/@VERSION@/${VER}/g" distribution.xml.in > distribution.xml
 pkgbuild --root ${RASCAL_PATH} --identifier com.rascal2.rascal.pkg --version ${VER} --install-location "/Applications/rascal.app" rascal.pkg
-productbuild --distribution distribution.xml --resources . ${NAME}
+productbuild --sign ${DEV_TEAM_ID} --timestamp --distribution distribution.xml --resources . ${NAME}
