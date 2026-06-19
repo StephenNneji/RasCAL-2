7766c28cd9395623f36affccbf975ec22c1d5f93
StephenNneji <34302892+StephenNneji@users.noreply.github.com>
2026-06-19 14:30:48 +0100

    Update make.sh

--- packaging/macos/make.sh
+++ packaging/macos/make.sh
@@ -4,6 +4,9 @@ RASCAL_PATH="../bundle/rascal.app"
 VER_NAME="$1"
 ARCH_NAME="$2"
 DEV_TEAM_ID="$3"
+API_CONNECT_KEY_ID="$4"
+NOTARY_ID="$5"
+NOTARY_PASSWORD="$6"
 VER="main"
 
 if [[ ${VER_NAME:0:1} == 'v' ]]; then
@@ -25,3 +28,7 @@ codesign -v --deep --force --timestamp --options=runtime --entitlements ./entitl
 sed -e "s/@VERSION_NAME@/${VER_NAME}/g" -e "s/@VERSION@/${VER}/g" distribution.xml.in > distribution.xml
 pkgbuild --root ${RASCAL_PATH} --identifier com.rascal2.rascal.pkg --version ${VER} --install-location "/Applications/rascal.app" rascal.pkg
 productbuild --sign ${DEV_TEAM_ID} --timestamp --distribution distribution.xml --resources . ${NAME}
+
+# Notarise and staple
+xcrun notarytool submit --apple-id ${NOTARY_ID} --password ${NOTARY_PASSWORD} --team-id ${DEV_TEAM_ID} --key-id ${API_CONNECT_KEY_ID} --key ./auth_key.p8 --wait ${NAME} 
+xcrun stapler staple ${NAME}
